198f2eae7eefd5301b11a39d05681c0fddc1c075
Nishant Singh <nishaantkrsingh@gmail.com>
2025-08-10 14:58:58 +0530

    added functionality to send messages using webhooks

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const webhookUrl = process.env.WEBHOOK_URL; 
+const messageContent = 'Hello from Node.js!';
+
+async function sendDiscordMessage(content) {
+  try {
+    const response = await fetch(webhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        content: content,
+        username: 'Notification Bot',
+        // avatar_url: 'YOUR_AVATAR_URL',
+      }),
+    });
+
+    if (!response.ok) {
+      console.error(`Error sending message: ${response.status} ${response.statusText}`);
+    } else {
+      console.log('Message sent successfully!');
+    }
+  } catch (error) {
+    console.error('Error sending Discord message:', error);
+  }
+}
+
+sendDiscordMessage(messageContent);
